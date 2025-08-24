ceaaa868c014903c57ad07617dfec7b62a08b39f
cat <myakoven@student.42berlin.de>
2025-08-24 23:18:04 +0200

    feat(02): add draft of insert pent into main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -24,7 +24,7 @@ size_t lower_bound_index(const std::vector<int> &vec, size_t first, size_t last,
 	return first;
 }
 
-void insert_or_reorganize_for_num(std::pair<int, int> &element, std::vector<std::pair<int, int>> &numPairs, size_t index)
+void insert_or_reorganize_for_num(std::pair<int, int> &element, std::vector<std::pair<int, int> > &numPairs, size_t index)
 {
 	// we will compare element with pair at index
 	if (element.second >= numPairs[index].second)
@@ -38,7 +38,7 @@ void insert_or_reorganize_for_num(std::pair<int, int> &element, std::vector<std:
 	}
 }
 
-void recursive_insert_sort(std::vector<std::pair<int, int>> &numPairs, size_t index)
+void recursive_insert_sort(std::vector<std::pair<int, int> > &numPairs, size_t index)
 {
 	if (index < 1)
 		return;
@@ -51,7 +51,7 @@ void recursive_insert_sort(std::vector<std::pair<int, int>> &numPairs, size_t in
 	}
 }
 
-void create_main_and_pend(const std::vector<std::pair<int, int>> &unsortedPairs, std::vector<int> &main, std::vector<int> &pend)
+void create_main_and_pend(const std::vector<std::pair<int, int> > &unsortedPairs, std::vector<int> &main, std::vector<int> &pend)
 {
 	for (size_t i = 0; i < unsortedPairs.size(); i++)
 	{
@@ -60,22 +60,40 @@ void create_main_and_pend(const std::vector<std::pair<int, int>> &unsortedPairs,
 	}
 }
 
-void insert_pend_into_main(std::vector<int> &main, std::vector<int> &pend, std::vector<int> jacobs)
+void insert_pend_into_main(std::vector<int> &main, std::vector<int> &pend, std::vector<size_t> jacobs)
 {
+	size_t j;
+	size_t index;
+	j = 0;
+	main.insert(main.begin(), pend[0]); // insert before all numbers
+	j++;
+	while (jacobs[j] < pend.size())
+	{
+		index = jacobs[j];
+		while (index > jacobs[j - 1])
+		{
+			size_t maxIndexToSearch = jacobs[j - 1] * 2 + (jacobs[j] - jacobs[j - 1]);
+			size_t insertIndex = lower_bound_index(main, 0, maxIndexToSearch, pend[index]);
+			main.insert(main.begin() + insertIndex, pend[index]);
+			index--;
+		}
+		j++;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	long n1, n2;
+	long straggler = -3000000000;
 	size_t i;
-	std::vector<std::pair<int, int>> numPairs;
+	std::vector<std::pair<int, int> > numPairs;
 	std::vector<int> main;
 	std::vector<int> pend;
 
-	static size_t jacobsthal[] = {0, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731};
-	size_t jacobsthalSize = sizeof(jacobsthal) / sizeof(jacobsthal[0]);
+	static size_t jacobsArr[] = {0, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731};
+	size_t jacobsthalSize = sizeof(jacobsArr) / sizeof(jacobsArr[0]);
 
-	std::vector<int> jacobsthalV(jacobsthal, jacobsthal + jacobsthalSize);
+	std::vector<size_t> jacobsthal(jacobsArr, jacobsArr + jacobsthalSize);
 
 	char *endptr = NULL;
 
@@ -88,38 +106,49 @@ int main(int argc, char **argv)
 	while (argv[i])
 	{
 		n1 = strtol(argv[i], &endptr, 0);
-		if (*endptr != 0)
+		if (*endptr != 0 || n1 > INT_MAX)
 		{
-			std::cerr << "Error: not a number" << std::endl;
+			std::cerr << "Error: not an int" << std::endl;
 			return (1);
 		}
-		if (argv[i + 1])
+		if (!argv[i + 1])
 		{
-			n2 = strtol(argv[i + 1], &endptr, 0);
-			if (*endptr != 0)
-			{
-				std::cerr << "Error: not a number" << std::endl;
-				return (1);
-			}
+			straggler = static_cast<int>(n1);
+			break;
 		}
+		n2 = strtol(argv[i + 1], &endptr, 0);
+		if (*endptr != 0 || n1 > INT_MAX)
+		{
+			std::cerr << "Error: not an int" << std::endl;
+			return (1);
+		}
+
 		if (n2 > n1)
-			numPairs.push_back(std::make_pair<int, int>(n1, n2));
+			numPairs.push_back(std::make_pair<int, int>(static_cast<int>(n1), static_cast<int>(n2)));
 		else
-			numPairs.push_back(std::make_pair<int, int>(n2, n1));
+			numPairs.push_back(std::make_pair<int, int>(static_cast<int>(n2), static_cast<int>(n1)));
 		i++;
 	}
 	if (numPairs.size() > 1500)
 		std::cerr << "Error: too many numbers, but here are the results anyway" << std::endl;
 
-	recursive_insert_sort(numPairs, i - 1 - 1); // adjust for argv[0] && len - 1 for last index
+	/*
+	Ford Johnson Merge Insert Sort Algorithm
+	*/
+	recursive_insert_sort(numPairs, i - 1 - 1); // i - 1 - 1: adjust for argv[0] && len - 1: last index
 	create_main_and_pend(numPairs, main, pend);
-	insert_pend_into_main(pend, main, jacobsthalV);
-	// fordSort(unsortedNumberPairs); //TODO
-
-	// PRINT
-	for (i = 0; i < numPairs.size(); ++i)
+	insert_pend_into_main(pend, main, jacobsthal);
+	if (straggler >= INT_MIN && straggler < INT_MAX)
 	{
-		std::cout << numPairs[i] << " ";
+		size_t insertIndex = lower_bound_index(main, 0, main.size(), straggler);
+		main.insert(main.begin() + insertIndex, straggler);
 	}
-	std::cout << std::endl;
+
+	/*
+	Print
+	*/
+	for (i = 0; i < main.size(); ++i)
+	{
+		std::cout << main[i] << std::endl;
+	}
 }
